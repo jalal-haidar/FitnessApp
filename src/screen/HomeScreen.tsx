4c84de12ec08cda631a5e35ae1b71cbc38f9de1d
Jalal-Khan <jalalusafxai@gmail.com>
2023-12-25 00:40:21 +0500

    HomeSreen layout enhancement

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -13,14 +13,15 @@ const HomeScreen = () => {
         <View
             style={{
                 backgroundColor: '#FFFFFF',
-                flex: 1,
+                // flex: 1,
+                height: '100%',
                 // overflow: 'hidden',
             }}>
             {/* CHILDREN of MAIN VIEW */}
             <View
                 style={{
                     // backgroundColor: 'gold',
-                    flex: 1,
+                    height: '50%',
                     justifyContent: 'center',
                     alignItems: 'center',
                 }}>
@@ -35,6 +36,7 @@ const HomeScreen = () => {
                     style={{
                         alignItems: 'center',
                         rowGap: 20,
+                        // backgroundColor: 'black',
                         top: 30,
                     }}>
                     <Text style={styles.titleText}>Welcome To <Text style={styles.innerText}>Fitness Hoopligans</Text></Text>
@@ -70,8 +72,10 @@ const styles = StyleSheet.create({
         // marginBottom: 20,
     },
     lowerContainer: {
-        backgroundColor: '#FFFFFF',
+        // backgroundColor: '#FFFFFF',
+        backgroundColor: 'gold',
         flex: 1,
+        height: '50%',
         // borderTopWidth: 1,
         // borderTopColor: 'black',
         // borderBlockColor: 'black',
@@ -98,16 +102,16 @@ const styles = StyleSheet.create({
     },
 
     titleText: {
-        fontFamily: 'sans-serif',
-        // fontFamily: 'Arial',
-        fontSize: 25,
-        fontWeight: 'bold',
+        fontFamily: 'Urbanist',
+        fontSize: 24,
+        fontWeight: '700',
         color: 'black',
     },
     baseText: {
-        fontFamily: 'sans-serif',
-        fontWeight: 'bold',
-        color: 'black',
+        fontFamily: 'Urbanist',
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#000000',
     },
     innerText: {
         fontWeight: 'bold',
@@ -116,25 +120,26 @@ const styles = StyleSheet.create({
     button: {
         // backgroundColor: '#841584',
         // padding: 10,
-        borderRadius: 10,
+        borderRadius: 8,
         width: '75%',
         height: '9%',
-        borderWidth: 1.5,
-        borderColor: 'red',
+        borderWidth: 1,
+        borderColor: '#D00D0D',
         flexGrow: 1,
         justifyContent: 'center',
     },
     buttonText: {
-        color: 'black',
-        fontFamily: 'sans-serif',
+        fontFamily: 'Urbanist',
         fontSize: 16,
+        fontWeight: '500',
+        color: '#2F2F2F',
 
         alignSelf: 'center',
     },
     startButton: {
-        backgroundColor: 'black',
+        backgroundColor: '#1A1A1A',
         // padding: 10,
-        borderRadius: 10,
+        borderRadius: 8,
         width: '75%',
         height: '9%',
         borderWidth: 1.5,
@@ -143,9 +148,10 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     startButtomText: {
-        color: 'white',
-        fontFamily: 'sans-serif',
+        fontFamily: 'Urbanist',
+        color: '#FFFFFF',
         fontSize: 16,
+        fontWeight: '500',
 
         alignSelf: 'center',
     },
